8c6cfbb885a1a37065095570d056c99fe33dc36e
Jean dos Santos Silveira <jeanssilveira@ibm.com>
2024-02-26 09:08:12 -0300

    service layer, component registration

--- src/main/java/com/overtimejs/course/resources/UserResource.java
+++ src/main/java/com/overtimejs/course/resources/UserResource.java
@@ -1,17 +1,32 @@
 package com.overtimejs.course.resources;
 
 import com.overtimejs.course.entities.User;
+import com.overtimejs.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User user = new User(1L, "Jean", "jean@overtime.com", "9999999", "passpass");
-           return ResponseEntity.ok().body(user);
+    public ResponseEntity<List<User>> findAll() {
+
+        List<User> list = userService.findAll();
+           return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
