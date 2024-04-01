8eb704368522e2ab0deb9fa61dc56b55f3c53376
Jean dos Santos Silveira <jeanssilveira@ibm.com>
2024-04-01 09:16:19 -0300

    delete and user update

--- src/main/java/com/overtimejs/course/resources/UserResource.java
+++ src/main/java/com/overtimejs/course/resources/UserResource.java
@@ -38,4 +38,18 @@ public class UserResource {
 
         return ResponseEntity.created(uri).body(obj);
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+        obj = userService.update(id, obj);
+
+        return ResponseEntity.ok().body(obj);
+    }
 }
