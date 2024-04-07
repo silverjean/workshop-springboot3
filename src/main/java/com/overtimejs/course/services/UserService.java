42fffe04d1213c50e95de3c89fd1a122a469f965
Jean dos Santos Silveira <jeanssilveira@ibm.com>
2024-04-07 15:03:35 -0300

    exception handler

--- src/main/java/com/overtimejs/course/services/UserService.java
+++ src/main/java/com/overtimejs/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.overtimejs.course.services;
 
 import com.overtimejs.course.entities.User;
 import com.overtimejs.course.repositories.UserRepository;
+import com.overtimejs.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
---
+++ src/main/java/com/overtimejs/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.overtimejs.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. ID " + id);
+    }
+}
